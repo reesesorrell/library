a5570a41227dfbf8df4b982a09467466df74d4c4
Reese Sorrell <joreesesorrell@gmail.com>
2022-11-16 23:25:36 -0600

    Add DOM display functionality for book objects
    Change display function so that it displays the properties of a book
    object onto the html.

--- script.js
+++ script.js
@@ -25,9 +25,32 @@ function addBookToLibrary(title, author, pages, haveRead) {
 }
 
 function displayBooks(bookList) {
-    console.log('calling')
     for (let i = 0; i<bookList.length;  i++) {
-        console.log(bookList[i]);
+        var bookCard = document.querySelector('.bookHolder');
+
+        var bookDisplay = document.createElement('div');
+        bookDisplay.classList.add('book');
+        bookDisplay.setAttribute('id', 'book' + (i + 1));
+        bookCard.appendChild(bookDisplay)
+
+        var titleDisplay = document.createElement('div');
+        titleDisplay.innerHTML = bookList[i].title;
+        bookDisplay.appendChild(titleDisplay);
+
+        var authorDisplay = document.createElement('div');
+        authorDisplay.innerHTML = bookList[i].author;
+        bookDisplay.appendChild(authorDisplay);
+
+        var pageDisplay = document.createElement('div');
+        pageDisplay.innerHTML = bookList[i].pages;
+        bookDisplay.appendChild(pageDisplay);
+
+        var haveReadDisplay = document.createElement('div');
+        haveReadDisplay.innerHTML = bookList[i].haveRead;
+        bookDisplay.appendChild(haveReadDisplay);
     }
 }
 
+addBookToLibrary('Mistborn', 'Brandon', 900, true)
+addBookToLibrary('Elantris', 'Brandon', 300, true)
+displayBooks(userLibrary)
